e57c3858718639789726ec1fd1ef7e3530b154a9
sachinkamat <sachinkamat2004@outlook.com>
2024-01-15 03:36:46 +0530

    Added all the features

--- navayana_website/auth.py
+++ navayana_website/auth.py
@@ -12,6 +12,8 @@ from . import params,db
 from . import pages
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import login_user,logout_user,current_user,login_required
+import json
+
 
 
 
@@ -20,7 +22,7 @@ auth=Blueprint('auth',__name__)
 # Function used to direct user to the login page name value is used to input
 @auth.route('/login',methods=['GET','POST'])
 def login():
-  
+    #if 'user' in session or current_user:
     if request.method == 'POST':
         UserId=request.form.get('Email')
         entered_password=request.form.get('password')
@@ -35,7 +37,8 @@ def login():
             else:
                 return render_template('Login.html',error="Wrong password !")
         elif UserId == params['UserId'] and entered_password == params['admin_password']:
-            return redirect(url_for('pages.AdminDashboard',user=current_user))
+            session['user']=UserId
+            return redirect(url_for('pages.AdminDashboard'))
         else:
             return render_template('Login.html',error="User not found contact admin !")
     else: 
@@ -49,9 +52,42 @@ def logout():
     logout_user()
     return redirect(url_for('pages.homepage'))
 
+@auth.route('/Adminlogout')
+def Adminlogout():
+    if ('user' in session and session["user"] == params["UserId"]):
+        session.pop('user')
+        return redirect(url_for('pages.homepage'))
+    else:
+        return redirect(url_for('pages.home'))
+
 @auth.route('/change-admin-password',methods=['GET','POST'])
 def changeAdminpassword():
-    pass
+    if ('user' in session and session["user"] == params["UserId"]):
+        if request.method == 'POST':
+            oldpassword=request.form.get ('old_password')
+            newpassword=request.form.get('new_password')
+            confirm_password=request.form.get('confirm_password')
+
+            if  check_password_hash(params['admin_password'],oldpassword):
+                if len(newpassword)<8:
+                    return render_template('AdminPasswordC.html',error='Password must have a minimum of 8 character.')
+                elif newpassword != confirm_password:
+                    return render_template('AdminPasswordC.html',error="Passwords don't match")
+                else:
+                    new_passowrd_hash=generate_password_hash(newpassword,method='pbkdf2:sha256')
+                    with open(r'C:\Users\HP\Desktop\website\navayana_website\credentials.json','w') as c:
+                        json.dump(params, c, admin_password=new_passowrd_hash)
+                    
+                    flash("password changed successfully!",category="success")
+                    return redirect(url_for('pages.AdminDashboard'))
+            else:
+                return render_template('AdminPasswordC.html',error="Wrong Password!")
+
+        else:
+            return render_template('AdminPasswordC.html')
+        
+    else:
+        return redirect(url_for('pages.home'))
 
 
 
@@ -68,7 +104,7 @@ def change_password():
         user=current_user
         if  check_password_hash(user.password,oldpassword):
             if len(newpassword)<8:
-                return render_template('Password must have a minimum of 8 character.')
+                return render_template('changepassword.html',error='Password must have a minimum of 8 character.')
             elif newpassword != confirm_password:
                 return render_template('changepassword.html',error="Passwords don't match")
             else:
@@ -88,47 +124,79 @@ def change_password():
 
 # Function used to add members, it checks for all requesties of the member and submits the request based on 
 # the condition and user action.
-@auth.route('/signup',methods=['GET','POST'])
+@auth.route('/AddAuthor',methods=['GET','POST'])
+def AddAuthor():
+    if ('user' in session and session["user"] == params["UserId"]):
+        if request.method == 'POST':
+            fullname=request.form.get('fullname')
+            uemail=request.form.get('email')
+            upassword=request.form.get('password')
+            confirm_password=request.form.get('confirm_password')
 
-def AddMember():
-    if request.method == 'POST':
-        fullname=request.form.get('fullname')
-        uemail=request.form.get('email')
-        upassword=request.form.get('password')
-        confirm_password=request.form.get('confirm_password')
-
-        user=Users.query.filter_by(email=uemail).first()
-        if user:
-            flash('Email Already registered !',category='error')
-        elif len(fullname) <= 5:
-            flash('Full Name must be greate than 5 letters',category='error')
-        elif len(uemail) <= 8:
-            flash('Enter proper Email',category='error')
-        elif len(upassword)< 8:
-            flash('Password must be at least 8 letters',category='error')
-        elif upassword !=confirm_password:
-            flash('Password does not match',category='error')
-        else:
-            user=Users(name=fullname,email=uemail,password=generate_password_hash(upassword,method='pbkdf2:sha256'))
-            db.session.add(user)
-            db.session.commit()
-            flash('Member added!!',category='success')
-
-        return redirect(url_for('pages.AdminDashboard'))
+            user=Users.query.filter_by(email=uemail).first()
+            if user:
+                flash('Email Already registered !',category='error')
+            elif len(fullname) <= 5:
+                flash('Full Name must be greate than 5 letters',category='error')
+            elif len(uemail) <= 8:
+                flash('Enter proper Email',category='error')
+            elif len(upassword)< 8:
+                flash('Password must be at least 8 letters',category='error')
+            elif upassword !=confirm_password:
+                flash('Password does not match',category='error')
+            else:
+                user=Users(name=fullname,email=uemail,password=generate_password_hash(upassword,method='pbkdf2:sha256'))
+                db.session.add(user)
+                db.session.commit()
+                flash('Member added!!',category='success')
+            return redirect(url_for('pages.AdminDashboard'))
     
+        else:
+            return render_template('AddAuthor.html')
     else:
-        return render_template('AddMember.html')
+        return redirect(url_for('pages.home'))
 
 
 
-@auth.route('/delete_user')
-@login_required
-def DeleteMember():
-    pass
+@auth.route('/DeleteAuthor/<int:author_id>')
+def DeleteAuhtor(author_id):
+    if ('user' in session and session["user"] == params["UserId"]):
+        user=Users.query.filter_by(id=author_id).first()
+        db.session.delete(user)
+        db.session.commit()
+        flash('User Deleted Successfully !!',category='success')
+        return redirect(url_for('pages.AdminDashboard'))
+    else:
+        return render_template('pages.homepage')
+        
 
-@auth.route('/edit_user')
-@login_required
-def EditMember():
-    pass
+@auth.route('/EditAuthor',methods=['GET','POST'])
+def EditAuthor():
+    if ('user' in session and session["user"] == params["UserId"]):
+        if request.method == 'POST':
+            uemail=request.form.get('email')
+            user1=Users.query.filter_by(email=uemail).first()
+            if user1:
+                newpassword=request.form.get('new_password')
+                confirm_password=request.form.get('confirm_password')
+
+                user=user1
+                if len(newpassword)<8:
+                    return render_template('Password must have a minimum of 8 character.')
+                elif newpassword != confirm_password:
+                    return render_template('EditAuthor.html',error="Passwords don't match")
+                else:
+                    new_passowrd_hash=generate_password_hash(newpassword,method='pbkdf2:sha256')
+                    user.password=new_passowrd_hash
+                    db.session.commit()
+                    flash("password changed successfully!",category="success")
+                    return redirect(url_for('pages.AdminDashboard'))
+            else:
+                return render_template('EditAuthor.html',error="No author with that email")
+        else:
+            return render_template('EditAuthor.html')
+    else:
+        flash("Invalid request found",category="error")
+        return redirect(url_for('pages.AdminDashboard'))
 
 
--- navayana_website/pages.py
+++ navayana_website/pages.py
@@ -44,7 +44,7 @@ def LocateUs():
 # this funciton used for testing.
 @pages.route('/test')
 def testing():
-    return render_template("AddMember.html")
+    return render_template("AddAuthor.html")
 
 # Redirects user to the Read article page based on 'article id'
 @pages.route('/readarticles/<int:article_id>',methods=['GET'])
@@ -61,9 +61,13 @@ website users visit.
 
 """
 @pages.route('/admindashboard')
-@login_required
 def AdminDashboard():
-    return render_template('AdminPage.html')
+    if ('user' in session and session["user"] == params["UserId"]):
+        articles=Articles.query.filter_by().all()
+        users=Users.query.filter_by().all()
+        return render_template('AdminPage.html',articles=articles,users=users)
+    else:
+        return redirect(url_for('pages.homepage'))
 
 @pages.route('/dashboard')
 @login_required
@@ -73,6 +77,7 @@ def Dashboard():
 
 # This method id used to upload articles to the data base with add the neccessary requirements 
 @pages.route('/addarticle',methods=['GET','POST'])
+@login_required
 def addarticle():
         user=current_user
         if request.method=='POST':
@@ -82,9 +87,9 @@ def addarticle():
             
             now = datetime.now()
             formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
-            Email=user.userid
+            authorid=current_user.id
             Name=user.name
-            article=Articles(title=Title,description=Description,content=Content_uploaded,Last_updated=formatted_date,date_posted=formatted_date,author_email=Email,author=Name)
+            article=Articles(title=Title,description=Description,content=Content_uploaded,Last_updated=formatted_date,date_posted=formatted_date,author=Name,author_id=authorid)
             
             db.session.add(article)
             db.session.commit()
@@ -93,44 +98,64 @@ def addarticle():
         else:
             return render_template('AddArticle.html')
 
-@pages.route('/Edit-Article/<int:articleid>')
+@pages.route('/EditArticle/<int:articleid>',methods=['GET','POST'])
 @login_required
 def EditArticle(articleid):
-    
-        article_to_edit=Articles.query.filter_by(articleid=articleid).first()
-        if article_to_edit.author_email == user.email :
-            if request.method=="POST":
-                Title=request.form.get('title')
-                Description=request.form.get('description')
-                content_edited=request.form.get('content')
-                
-                now = datetime.now()
-                formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
+    user=current_user
+    article_to_edit=Articles.query.filter_by(articleid=articleid).first()
+    if article_to_edit.author_id == user.id :
+        if request.method=="POST":
+            Title=request.form.get('title')
+            Description=request.form.get('description')
+            content_edited=request.form.get('content') 
+            now = datetime.now()
+            formatted_date = now.strftime("%Y-%m-%d %H:%M:%S")
 
-                article_to_edit.title=Title
-                article_to_edit.description=Description
-                article_to_edit.content=content_edited
-                article_to_edit.Last_updated=formatted_date
+            article_to_edit.title=Title
+            article_to_edit.description=Description
+            article_to_edit.content=content_edited
+            article_to_edit.Last_updated=formatted_date
                 
-                db.session.commit()
-                return redirect(url_for(Dashboard))
+            db.session.commit()
+            flash('Aritcle updated success fully',category='success')
+            return redirect(url_for('pages.Dashboard'))
             
-            else:
-                return render_template('EditArticles.html',article=article_to_edit)
         else:
-            flash('You are no author of the article, You cannot edit the post')
-            return render_template(url_for(Dashboard))
+            return render_template('EditArticles.html',article=article_to_edit)
+    else:
+        flash('You are no author of the article, You cannot edit the post')
+        return render_template(url_for('pages.Dashboard'))
 
-"""
-        else:
-            flash("Not authorized to edit article",category='error')
-            return redirect(url_for(Dashboard))
-"""
 
-@pages.route('/Delete-Article/<int:articleid>')
+
+@pages.route('/DeleteArticle/<int:articleid>')
 @login_required
-def DeleteArticle(articleid):  
-    pass
+def DeleteArticle(articleid):
+    article=Articles.query.filter_by(articleid=articleid).first()
+    if article.author_id == current_user.id:  
+        db.session.delete(article)
+        db.session.commit()
+        flash("Article  Deleted Successfully",category='sucess')
+        return redirect(url_for('pages.Dashboard'))
+    else:
+        flash("Article Not Deleted !!!  You are not the auhtor of the article",category='error')
+        return redirect(url_for('pages.Dashboard'))
         
+
+@pages.route('/AdminDeleteArticle/<int:articleid>')
+def AdminDeleteArticle(articleid):
+    if ('user' in session and session["user"] == params["UserId"]):
+        article=Articles.query.filter_by(articleid=articleid).first()
+        db.session.delete(article)
+        db.session.commit()
+        flash('Article Deleted Successfully !!',category='success')
+        return redirect(url_for('pages.AdminDashboard'))
+    else:
+        return render_template('pages.homepage')
+    
+
+
+
+
 
 
